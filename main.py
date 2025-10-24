57086994a7d7308702b3798193b9b6f5bdfb0770
LordWorm1996 <dimitrios.psaroudakis@gmail.com>
2025-10-24 15:16:19 +0200

    added first music impl

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import pygame
 import sys
 
+import fonts
+import enemies
+import menus
+import music
+import skilltree
+import sound
+import variables
+import weapons
+
+
 # Initialize Pygame
 pygame.init()
 
 # Set up the fullscreen display
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 pygame.display.set_caption("Fullscreen Red Window")
+
+music.play_theme
 
 # Main loop
 running = True
@@ -19,6 +31,13 @@ while running:
             if event.key == pygame.K_ESCAPE:  # Exit on ESC key
                 running = False
 
+        elif event.key == pygame.K_p:
+            if is_paused:
+                music.unpause_theme()
+                is_paused = False
+            else:
+                music.pause_theme()
+                is_paused = True
     # Fill the screen with red (RGB: 255, 0, 0)
     screen.fill((255, 0, 0))
 
@@ -26,5 +45,7 @@ while running:
     pygame.display.flip()
 
 # Clean up
+music.stop_theme()
+music.unload_theme()
 pygame.quit()
 sys.exit()
---
+++ music.py
@@ -0,0 +1,29 @@
+import pygame
+
+main_theme = "sound_files/test_music.mp3"
+
+
+def init_music():
+    pygame.mixer.init()
+    pygame.mixer.music.load(main_theme)
+    pygame.mixer.music.set_volume(1.0)
+
+
+def play_theme():
+    pygame.mixer.music.play(-1, 0.0)
+
+
+def pause_theme():
+    pygame.mixer.music.pause()
+
+
+def unpause_theme():
+    pygame.mixer.music.unpause()
+
+
+def stop_theme():
+    pygame.mixer.music.stop()
+
+
+def unload_theme():
+    pygame.mixer.music.unload()
