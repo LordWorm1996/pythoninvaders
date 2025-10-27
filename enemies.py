225845d009bedcfaebfd84af3e289824c37f1394
LordWorm1996 <dimitrios.psaroudakis@gmail.com>
2025-10-27 18:15:02 +0100

    added enemy + boss functions

--- enemies.py
+++ enemies.py
@@ -1,1 +1,41 @@
 import pygame
+
+def red_enemy():
+
+def yellow_enemy():
+
+
+def orange_enemy():
+
+
+def green_enemy():
+
+
+def blue_enemy():
+
+
+def purple_enemy():
+
+
+def special_enemy():
+
+
+def red_boss():
+
+
+def yellow_boss():
+
+
+def orange_boss():
+
+
+def green_boss():
+
+
+def blue_boss():
+
+
+def purple_boss():
+
+
+def final_boss():
