4f935d4c3442654f868db701d73048f0a081a654
LordWorm1996 <dimitrios.psaroudakis@gmail.com>
2025-11-05 10:26:38 +0100

    added varibles and enemy stats

--- enemies.py
+++ enemies.py
@@ -1,57 +1,87 @@
+from tkinter import Variable
 import pygame
+import variables
 
 
 def red_enemy():
-    print("enemy")
+    health = variables.health
+    speed = variables.speed
+    damage = variables.damage
 
 
 def yellow_enemy():
-    print("enemy")
+    health = variables.health
+    speed = variables.speed * 5
+    damage = variables.damage - 0.5
 
 
 def orange_enemy():
-    print("enemy")
+    health = variables.health * 10
+    speed = variables.speed - 0.5
+    damage = 0
 
 
 def green_enemy():
-    print("enemy")
+    health = variables.health + 1
+    speed = variables.speed + 1
+    damage = variables.damage + 1
 
 
 def blue_enemy():
-    print("enemy")
+    health = variables.health + 5
+    speed = variables.speed + 2
+    damage = variables.damage + 2
 
 
 def purple_enemy():
-    print("enemy")
+    health = variables.health + 10
+    speed = variables.speed + 2
+    damage = variables.damage + 5
 
 
 def special_enemy():
-    print("enemy")
+    health = variables.health * variables.random_multiplier
+    speed = variables.speed * variables.random_multiplier
+    damage = variables.damage * variables.random_multiplier
 
 
 def red_boss():
-    print("enemy")
+    health = variables.health * variables.boss_multiplier
+    speed = variables.speed
+    damage = variables.damage
 
 
 def yellow_boss():
-    print("enemy")
+    health = variables.health * variables.boss_multiplier
+    speed = variables.speed * 5
+    damage = variables.damage - 0.5
 
 
 def orange_boss():
-    print("enemy")
+    health = variables.health * 5 * variables.boss_multiplier
+    speed = variables.speed
+    damage = variables.damage
 
 
 def green_boss():
-    print("enemy")
+    health = variables.health * variables.boss_multiplier
+    speed = variables.speed + 2
+    damage = variables.damage + 2
 
 
 def blue_boss():
-    print("enemy")
+    health = variables.health * variables.boss_multiplier
+    speed = variables.speed + 5
+    damage = variables.damage + 5
 
 
 def purple_boss():
-    print("enemy")
+    health = variables.health * variables.boss_multiplier
+    speed = variables.speed + 5
+    damage = variables.damage + 10
 
 
 def final_boss():
-    print("enemy")
+    health = variables.health * variables.final_boss_multiplier
+    speed = variables.speed + 5
+    damage = variables.damage * variables.random_multiplier
--- variables.py
+++ variables.py
@@ -1,1 +1,23 @@
 import pygame
+import random
+
+health = 1
+damage = 1
+speed = 1
+health_regen = 0
+ultimate_regen = 1
+ultimate_full = 100
+
+random_multiplier = random.randint(1, 10)
+elite_enemy_multiplier = 10
+boss_multiplier = 100
+final_boss_multiplier = 500
+
+health_pack = 1
+big_health_pack = 2
+ultimate_pack = 10
+big_ultimate_pack = 20
+health_booster = 1
+health_regen_booster = 1
+ultimate_regen_booster = 1
+ultimate_boost = -1
